8d2817acd525b9fbf397fca5203a478681e56c87
your name <your email>
2018-04-20 12:14:40 +0800

    优化自动生成代码

--- gzxant-code/src/main/java/com/gzxant/code/generate/CodeGenerate.java
+++ gzxant-code/src/main/java/com/gzxant/code/generate/CodeGenerate.java
@@ -1,6 +1,8 @@
 package com.gzxant.code.generate;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,33 +14,50 @@ import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
+/**
+ * 代码生成器
+ *
+ * @author xiaoyc
+ * @since 2018-4-19
+ */
 public class CodeGenerate {
-	private static String[] tableName = {"shop_goods_sku"};
-	public static String author = "xiaoyc";
-	public static String dataType = "base"; // tree, crud, base
-	public static String output = "E:\\temp\\code-generate"; 
+	// TODO 用扩展的方式实现自定义 mybatis plus
 
-//	@Value("${spring.datasource.url}")
-	public static String url = "jdbc:mysql://localhost:3306/gzxant?useUnicode=true&characterEncoding=utf8&useSSL=false";
-//	@Value("${spring.datasource.username}")
-	public static String name = "root";
-//	@Value("${spring.datasource.password}")
-	public static String passWord = "123456";
-//	@Value("${spring.datasource.driver-class-name}")
+	// --------------变动参数区 start------------------------------------------
+	/* === 表名 === */
+	private static String[] tableNames = { "fs_information" };
+	/* === 开发者 === */
+	public static String author = "莫进生";
+	/* === 输出目标项目，为空则生成在当前项目中 === */
+	public static String targetProject = "gzxant-food-safety-web";
+	/* === 准备生成文件[controller, service, dao, entity, xml] === */
+	public static List<String> beGenerateFile = new ArrayList<>(
+			Arrays.asList("controller", "service", "dao", "entity", "xml"));
+	// --------------变动参数区 end------------------------------------------
+
+	// --------------数据源配置区 start------------------------------------------
+	public static String url = "jdbc:mysql://192.168.0.200:3306/gzxant?useUnicode=true&characterEncoding=utf8&useSSL=false";
+	public static String name = "gzxant";
+	public static String passWord = "gzxant1804";
 	public static String driver = "com.mysql.jdbc.Driver";
-	
+	// --------------数据源配置区 end------------------------------------------
 
 	public static void main(String[] args) {
-		
+		String output = "";
+
 		AutoGenerator mpg = new AutoGenerator();
 		// 选择 freemarker 引擎，默认 Veloctiy
-//		mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+		// mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+
+		CodeGenerate cg = new CodeGenerate();
+		output = cg.getPath(targetProject) + "\\src\\main\\java";
 
 		// 全局配置
 		GlobalConfig gc = new GlobalConfig();
@@ -57,12 +76,14 @@ public class CodeGenerate {
 		gc.setServiceImplName("%sService");
 		gc.setControllerName("%sController");
 		mpg.setGlobalConfig(gc);
-		
+
 		// 自定义包配置
 		PackageConfig pg = new PackageConfig();
+		pg.setParent("com.gzxant");
 		pg.setController("controller");
+		pg.setMapper("dao");
 		pg.setXml("xml");
-		
+
 		mpg.setPackageInfo(pg);
 
 		// 数据源配置
@@ -88,26 +109,15 @@ public class CodeGenerate {
 		// strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
 		// strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
 		strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-		strategy.setInclude(tableName); // 需要生成的表
+		strategy.setInclude(tableNames); // 需要生成的表
 		// strategy.setExclude(new String[]{"test"}); // 排除生成的表
 		// 自定义实体父类
 		// 自定义实体，公共字段
 		// strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
-		// 自定义 mapper 父类
-		if (dataType.equals("tree")) {
-			strategy.setSuperEntityClass("com.gzxant.base.entity.TreeEntity");
-			strategy.setSuperMapperClass("com.gzxant.base.dao.TreeDao");
-		} else if (dataType.equals("crud")) {
-			strategy.setSuperEntityClass("com.gzxant.base.entity.DataEntity");
-			strategy.setSuperMapperClass("com.gzxant.base.dao.CrudDao");
-		} else if (dataType.equals("base")) {
-			strategy.setSuperEntityClass("com.gzxant.base.entity.BaseEntity");
-			strategy.setSuperMapperClass("com.gzxant.base.dao.BaseDao");
-		}
 		// 自定义 service 父类
 		strategy.setSuperServiceClass("com.gzxant.base.service.IBaseService");
 		// 自定义 service 实现类父类
-		strategy.setSuperServiceImplClass("om.gzxant.base.service.impl.BaseService");
+		strategy.setSuperServiceImplClass("com.gzxant.base.service.impl.BaseService");
 		// 自定义 controller 父类
 		strategy.setSuperControllerClass("com.gzxant.base.controller.BaseController");
 		// 【实体】是否生成字段常量（默认 false）
@@ -117,12 +127,6 @@ public class CodeGenerate {
 		// public User setName(String name) {this.name = name; return this;}
 		// strategy.setEntityBuilderModel(true);
 		mpg.setStrategy(strategy);
-
-		// 包配置
-		PackageConfig pc = new PackageConfig();
-		pc.setParent("com.gzxant");
-//		pc.setModuleName("shop");
-		mpg.setPackageInfo(pc);
 
 		// 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
 		InjectionConfig cfg = new InjectionConfig() {
@@ -134,44 +138,79 @@ public class CodeGenerate {
 			}
 		};
 
-		// 自定义 xxList.jsp 生成
+		// 页面 生成
 		List<FileOutConfig> focList = new ArrayList<FileOutConfig>();
-//		focList.add(new FileOutConfig("/template/list.jsp.vm") {
-//			@Override
-//			public String outputFile(TableInfo tableInfo) {
-//				// 自定义输入文件名称
-//				return "D://my_" + tableInfo.getEntityName() + ".ftl";
-//			}
-//		});
-//		cfg.setFileOutConfigList(focList);
-		mpg.setCfg(cfg);
+		// focList.add(new FileOutConfig("/template/list.ftl.vm") {
+		// @Override
+		// public String outputFile(TableInfo tableInfo) {
+		// // 自定义输入文件名称
+		// return output + "\\html\\" + tableInfo.getEntityName() + "\\list.ftl";
+		// }
+		// });
+		//
+		// focList.add(new FileOutConfig("/template/detail.ftl.vm") {
+		// @Override
+		// public String outputFile(TableInfo tableInfo) {
+		// // 自定义输入文件名称
+		// return output + "\\html\\" + tableInfo.getEntityName() + "\\detail.ftl";
+		// }
+		// });
 
-		// 调整 xml 生成目录演示
-		focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
-			@Override
-			public String outputFile(TableInfo tableInfo) {
-				return "/develop/code/xml/" + tableInfo.getEntityName() + ".xml";
-			}
-		});
 		cfg.setFileOutConfigList(focList);
 		mpg.setCfg(cfg);
 
+		// 调整 xml 生成目录
+		if (!beGenerateFile.isEmpty() && beGenerateFile.contains("xml")) {
+			focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+				@Override
+				public String outputFile(TableInfo tableInfo) {
+					CodeGenerate cg = new CodeGenerate();
+					String path = cg.getPath("gzxant-common") + "\\src\\main\\resources\\mapper";
+					String[] names = tableInfo.getName().split("_");
+					for (int i = 0; i < names.length; i++) {
+						path = path + "\\" + names[i];
+					}
+
+					path = path + "\\" + tableInfo.getEntityName() + "Dao.xml";
+					System.out.println("dao path : " + path);
+					return path;
+				}
+			});
+			cfg.setFileOutConfigList(focList);
+		}
+		mpg.setCfg(cfg);
+
 		// 关闭默认 xml 生成，调整生成 至 根目录
-//		TemplateConfig tc = new TemplateConfig();
-//		tc.setXml(null);
-//		mpg.setTemplate(tc);
+		// TemplateConfig tc = new TemplateConfig();
+		// tc.setXml(null);
+		// mpg.setTemplate(tc);
 
 		// 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
 		// 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
-//		 TemplateConfig tc = new TemplateConfig();
-//		 tc.setController("/templates/code/controllerTemplate.ftl");
-//		 tc.setEntity(null);
-//		 tc.setMapper("...");
-//		 tc.setXml(null);
-//		 tc.setService("...");
-//		 tc.setServiceImpl("...");
+		TemplateConfig tc = new TemplateConfig();
+		if (!beGenerateFile.isEmpty() && beGenerateFile.contains("controller")) {
+			tc.setController("/controller.java.vm");
+		} else {
+			tc.setController("");
+		}
+
+		if (!beGenerateFile.isEmpty() && beGenerateFile.contains("service")) {
+			tc.setServiceImpl("/serviceImpl.java.vm");
+		} else {
+			tc.setServiceImpl("");
+			tc.setService("");
+		}
+
+		tc.setXml("");
+
+		if (!beGenerateFile.isEmpty() && !beGenerateFile.contains("entity")) {
+			tc.setEntity("");
+		}
+		if (!beGenerateFile.isEmpty() && !beGenerateFile.contains("dao")) {
+			tc.setMapper("");
+		}
 		// 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
-//		 mpg.setTemplate(tc);
+		mpg.setTemplate(tc);
 
 		// 执行生成
 		mpg.execute();
@@ -181,5 +220,20 @@ public class CodeGenerate {
 
 	}
 
+	/**
+	 * 获取目标项目所在目录
+	 *
+	 * @param targetProject
+	 *            为空时默认为当前项目
+	 * @return
+	 */
+	public String getPath(String targetProject) {
+		String path = (new File(this.getClass().getResource("/").getPath())).getAbsolutePath();
+		path = path.replace("\\target\\classes", "");
+		if (targetProject != null && !targetProject.equals("")) {
+			path = path.replace("gzxant-code", targetProject);
+		}
 
+		return path;
+	}
 }
