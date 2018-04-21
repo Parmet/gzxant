986099a6c762c5212217b176f32fc26869affbc8
zhongtao <zhongtao@520it.com>
2018-04-21 10:05:21 +0800

    更新到shop启动项目

--- gzxant-shop/src/main/java/com/gzxant/config/swagger/SwaggerConfig.java
+++ gzxant-shop/src/main/java/com/gzxant/config/swagger/SwaggerConfig.java
@@ -2,12 +2,8 @@
 package com.gzxant.config.swagger;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -18,6 +14,9 @@ import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chen on 2017/4/19.
